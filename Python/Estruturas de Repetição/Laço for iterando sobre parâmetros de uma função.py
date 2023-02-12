6d63b3f39c835a9f7a7f1aca925ec0d0df66ec81
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2023-02-12 17:15:21 -0300

    Delete Python directory

--- Python/Estruturas de Repetição/Laço for iterando sobre parâmetros de uma função.py
+++
@@ -1,7 +0,0 @@
-def cadastra(**kwargs):
-  for key, value in kwargs.items():
-    print(f'Parâmetro {key}, Dado/Valor: {value}')
-
-gerente = cadastra(Nome = 'Fernando',
-                   Credencial = 3509,
-                   Email = 'fernando2rad@gmail.com')
