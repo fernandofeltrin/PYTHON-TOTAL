9bf22ea9bfe2f9ec5289efa1b23f5f02c8be1346
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2024-02-06 00:31:40 -0300

    Create Agrupando registros com base em algum critério para uma variável-coluna temporária (GROUP BY).py

---
+++ LIVROS/Bancos de Dados Relacionais com Python e SQL/Agrupando registros com base em algum critério para uma variável-coluna temporária (GROUP BY).py
@@ -0,0 +1,44 @@
+import sqlite3
+
+conexao = sqlite3.connect('base.db')
+
+cursor = conexao.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Inventario (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Item TEXT,
+        Quantidade INTEGER,
+        Valor REAL
+    )
+""")
+
+dados = {'Itens': ['Tênis Nike', 'Camisa Adidas GG', 'Bermuda Umbro', 'Camisa Adidas P'],
+         'Quantidade': [5, 4, 1, 2],
+         'Valor': [149.90, 99.90, 69.90, 79.90]}
+
+dados_inventario = list(zip(dados['Itens'], dados['Quantidade'], dados['Valor']))
+
+cursor.executemany("INSERT INTO Inventario (Item, Quantidade, Valor) VALUES (?, ?, ?)", dados_inventario)
+
+cursor.execute("""
+    SELECT 'Camisa' AS categoria,
+           SUM(Quantidade) AS quantidade_total,
+           SUM(Valor * Quantidade) AS valor_total
+    FROM Inventario
+    WHERE Item LIKE 'Camisa%'
+    GROUP BY categoria
+""")
+
+resultado = cursor.fetchone()
+
+if resultado:
+    categoria, quantidade_total, valor_total = resultado
+    print(f'Total de camisas da marca Adidas: {quantidade_total}'
+          f'\nValor Total: R${valor_total:.2f}')
+else:
+    print("Nenhuma camisa encontrada no inventário.")
+
+conexao.commit()
+
+conexao.close()
